cb300912f2c96f24033cf7877bc3dddf586dad0a
Julio Añez <janez0266@gmail.com>
2021-06-15 03:20:20 -0300

    Agregadas las API Keys para Deploy

--- src/APIS/MarvelKey.js
+++ src/APIS/MarvelKey.js
@@ -1,9 +1,10 @@
 import md5 from "react-native-md5";
 import BackImg from "../images/marvel-characters.jpg";
 
-   
-const API_KEY_PUBLIC = process.env.REACT_APP_API_PUBLIC_KEY;
-const API_KEY_PRIVATE =process.env.REACT_APP_API_PRIVATE_KEY;
+// const API_KEY_PUBLIC = process.env.REACT_APP_API_PUBLIC_KEY;
+// const API_KEY_PRIVATE =process.env.REACT_APP_API_PRIVATE_KEY;
+const API_KEY_PUBLIC = "4450ccfed9a33ce8fc53c2227194cf45"
+const API_KEY_PRIVATE ="bcc0ab14311110d3d8bb768765bcb9f75d65d7d7";
 const API_TIMESTAMP = new Date().toISOString();
 const API_HASH = md5.hex_md5(API_TIMESTAMP + API_KEY_PRIVATE + API_KEY_PUBLIC);
 const URL_STRING_KEY = "apikey=" + API_KEY_PUBLIC + "&ts=" + API_TIMESTAMP + "&hash=" + API_HASH;
